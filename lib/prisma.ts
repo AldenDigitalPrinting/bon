a56ffa055d43186b29a676f4525a7d3845cbff61
alden <63831735+aldenasf@users.noreply.github.com>
2026-07-21 13:04:47 +0700

    fix(prisma): update PrismaClient import and configure adapter for PostgreSQL fix(schema): change Prisma client provider to 'prisma-client-js'

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const connectionString = process.env.DATABASE_URL;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
